5b5cb28604b149c35b2886ceb271a58b3357d600
aminsoltan <aminsoltani1881@gmail.com>
2023-03-20 01:52:07 +0330

    Tap gesture recognizer added

--- Task-3/ViewController.swift
+++ Task-3/ViewController.swift
@@ -1,19 +1,33 @@
 //
 //  ViewController.swift
-//  Task-3
+//  Task 3
 //
-//  Created by Amin Soltani on 12/29/1401 AP.
+//  Created by Amin Soltani on 12/20/1401 AP.
 //
 
 import UIKit
 
 class ViewController: UIViewController {
+    @IBOutlet weak var imageView: UIImageView!
 
+
+    
+ 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        let tap = UITapGestureRecognizer(target: self, action: #selector(touchedScreen(touch:)))
+            view.addGestureRecognizer(tap)
+        imageView.backgroundColor = .systemGreen
+        
+        
+        
     }
 
-
+    @objc func touchedScreen(touch: UITapGestureRecognizer) {
+        
+        let touchPoint = touch.location(in: self.view)
+    }
+    
 }
 
