62f1b7e307ce4025d60de80502f5463b8a3f6e45
Amin <110132827+aminsoltanii@users.noreply.github.com>
2023-03-20 01:46:01 +0330

    Initial files uploaded

---
+++ Task-3/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Task-3
+//
+//  Created by Amin Soltani on 12/29/1401 AP.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
